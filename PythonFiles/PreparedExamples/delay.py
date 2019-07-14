14025b16d6809fb54a2a00ee2a81491bb3deea53
WilrZac <zactowbesmusic@gmail.com>
2019-07-14 19:05:44 -0400

    troubleshooting

--- PythonFiles/PreparedExamples/delay.py
+++ PythonFiles/PreparedExamples/delay.py
@@ -7,19 +7,45 @@ import numpy as np
 # The simplest delay has this property: 
 # 
 # For each time step, the sample n samples ago is output.
+samples = [0, 1, 2, 3, 4, 5, 6, 7]
+n = 3
 
 def sample_delay(samples, n):
     """ delays the signal `samples` by n samples """
     out = [] 
-    for sample_number in range(len(samples) + n): 
+
+    for sample_number in range(len(samples) + n):  # sample_number = 0
         if sample_number <= n: 
             out.append(0)
         else:
             out.append(samples[sample_number - n])
     return out
     
-print(sample_delay([0, 1, 2, 3, 4, 5, 6, 7], 3))
 # this should output [0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7].
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ### The Standard Delay ### 
@@ -32,6 +58,10 @@ print(sample_delay([0, 1, 2, 3, 4, 5, 6, 7], 3))
 
 ### The seconds delay ###
 # This delay will take n in seconds rather than samples. To achieve this, we need another argument: the sampling rate. 
+
+# SR: samples/seconds
+# time_seconds: seconds
+# SR * time_samples: samples
 
 def seconds_delay(samples, time_seconds, SR):
     """ delays the input samples by time_seconds, given sampling rate SR """ 
@@ -49,6 +79,27 @@ def seconds_delay(samples, time_seconds, SR):
 # We'll notice that this is identical to sample_delay() save the modified arguments and the first line, converting time_seconds to n (samples)
     
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ### The Mix Delay ###
 # This delay will implement our second idea -- the mix knob. If the mix is 0.0, the output will be identical to the input. If the mix is 1.0, the output will be just the delay. 
 
@@ -56,7 +107,10 @@ def mix_delay(samples, time_seconds, SR, mix):
     """ delays the input samples by time_seconds, given sampling rate SR, with mix controlled by mix.""" 
     n = time_seconds * SR
     
-    dry_mix = (mix * -1) + 1
+    dry_mix = (mix * -1.0) + 1.0
+
+    # if mix is 1.0: 0.0
+    # if mix is 0.0: 1.0
 
     out = []
     for sample_number in range(len(samples) + n): 
@@ -68,25 +122,45 @@ def mix_delay(samples, time_seconds, SR, mix):
     return out
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ### The Feedback Delay ###
 # The feedback delay represents a problem that cannot be implemented the same way our previous delays were. 
 # While it's possible to do so, it's needlessly complicated and not particularly useful to learn.
 # Instead, we'll use a common method for implementing delays in real-time: circular buffers. 
 
+# samples = [0, 1, 2, 3, 4, 5, 6, 7] 
+# n = 1
+# mix = 0.5
+# feedback = 0.5
+
 def feedback_delay(samples, time_seconds, SR, mix, feedback):
     """ delays the input samples by time_seconds, given sampling rate SR, with mix controlled by mix.""" 
     n = time_seconds * SR
     
-    dry_mix = (mix * -1) + 1
+    dry_mix = (mix * -1.) + 1.
 
     out = []
     for sample_number in range(len(samples) + n): 
         if sample_number <= n: 
             out.append(samples[sample_number] * dry_mix)
         else:
+
             next_out = 0.
-            
-            for loop in range(sample_number / n):
+            for loop in range(sample_number / n): 
                 # `sample_number / n` is the number of times so far that we've reached a place we need to feedback -- the fist delay cycle will not have any feedback because there is not any delayed material to feedback.
                 next_out += samples[sample_number - (loop * n)] * (feedback ** loop)
                 # this prepares the next output value -- we can explain this on the board.
@@ -96,7 +170,19 @@ def feedback_delay(samples, time_seconds, SR, mix, feedback):
     return out
 
 
+
+
+
+
+
 # This implementation is pretty clunky and slow. It's not intuitive to our sense of feedback. This also is slower because we have to loop through the same feedback samples repeatedly. For instance, take a delay of `n = 4410` samples at `SR = 44100`, and if our samples buffer is 441,000 samples (10 seconds) long, when we reach the sample `sample_number = 4410 * 5 = 22050`, our inner for loop will call up the samples 4410 samples ago 5 times -- 17640, 13230, 8820, 4410, and 0. In the next example, we solve this more intuitively and in a way that is more similar to normal real-time implementations.
+
+
+
+
+
+
+
 
 ### The Circular Buffer Delay ### 
 # The circular buffer is a list that represents the delayed samples. Here's how we use it to make a delay. 
@@ -109,19 +195,24 @@ def circbuff_delay(samples, time_seconds, SR, mix, feedback):
     dry_mix = (mix * -1) + 1
     out = []
 
-    circbuff = np.zeros(n * 2) # make circbuff twice the length of the delay time -- it doesn't need to be that long but there's no harm.
-    write = n + 1 # put write in the middle of circbuff
-    read = write - n
     bufflength = n * 2
+    circbuff = np.zeros(n * 2) # make circbuff twice the length of the delay time -- it doesn't need to be that long but there's no harm.
 
-    for sample in samples: 
-        circbuff[write] = sample + (feedback * circbuff[write])
+    write = n + 1 # put write in the middle of circbuff 
+    read = write - n # n samples apart [write          read          ]
+    
+    for sample in samples: # 0
+        # circbuff[read] delayed
+        # circbuff[write] the sample we're about to set 
+        circbuff[write] = sample + (feedback * circbuff[read])
         out.append((circbuff[read] * mix) + (sample * dry_mix))
 
         write += 1 # increment write  
         read += 1 # increment read
         
+        # a % b  takes the remainder of a / b.
         write %= bufflength # if write is longer than the buffer's length, it will return it to the beginning of the buffer.
+        # write = write % bufflength
         read %= bufflength # ""
 
     return np.array(out)
@@ -134,10 +225,34 @@ def circbuff_delay(samples, time_seconds, SR, mix, feedback):
 
 
 y, sr = librosa.core.load('../../AudioFiles/KickOut.wav', mono=True, sr=44100)
-print(y)
-print('running delay')
-x = circbuff_delay(y, 0.05, sr, 0.5, 0.15)
 
-print(x)
+x = circbuff_delay(y, 0.2, sr, 0.5, 0.9)
 
+librosa.output.write_wav('./output.wav', x, sr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# load in audio file
+y, sr = librosa.core.load('../../AudioFiles/KickOut.wav', mono=True, sr=44100)
+
+
+# x is output of delay
+# write audio file to output.wav
 librosa.output.write_wav('./output.wav', x, sr)
